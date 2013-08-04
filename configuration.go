5bec25aea2c2f8d0b82ef7a1f7d5a98ef3b6f235
clinton <clinton.freeman@nicta.com.au>
2013-08-04 11:58:04 +1000

    Added basic configuration file in JSON.

---
+++ configuration.go
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) Clinton Freeman 2013
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Configuration struct {
+	OpticalFlowScale	float64
+	MovementThreshold	float64
+}
+
+func parseConfiguration(configFile string) (configuration Configuration, err error) {
+	// Create a default configuration.
+	config := Configuration{1000.0, 1.0}
+
+	// Open the configuration file.
+	file, err := os.Open(configFile)
+	if err != nil {
+		return config, err
+	}
+
+	// Parse the JSON in the configuration file.
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&config)
+	if err != nil {
+		return config, err
+	}
+
+	return config, nil
+}
